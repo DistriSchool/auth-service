285d29f48e84d4fdb18ce62ace2288693c2dd7a1
clys-man <clysmanalves@gmail.com>
2025-10-12 16:47:09 -0300

    feat: change health check endpoint response

---
+++ src/main/java/edu/unifor/br/distrischool/authservice/dto/StatusResponse.java
@@ -0,0 +1,15 @@
+package edu.unifor.br.distrischool.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusResponse {
+    private SystemStatus status;
+}
+
